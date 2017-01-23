f8fb562155d188c9931a742fcd777517ea4bb9e4
BrianLee <briankigondu@gmail.com>
2017-01-23 21:39:46 +0300

    Continue to find my way
    the npm module isn't as updated as github

--- index.js
+++ index.js
@@ -5,3 +5,19 @@ get twitter api key
 test api key
 
  */
+
+var config = require('./data/twitter_config');
+
+var Twitter = require('twitter-node-client').Twitter;
+
+var twitter = new Twitter(config);
+
+var error = function(err, response,body){
+	console.error(err);
+};
+var success = function(data){
+	console.log('Data [%s]', data)
+}
+
+
+twitter.getSearch({'q':'#100daysofcode', 'count':10}, error, success);
